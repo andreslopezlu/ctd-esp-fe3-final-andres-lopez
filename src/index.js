b2bf42aadcb3cc374614f4b6e16d6a21b1d9d949
andreslopezlu <andreslopezlu@gmail.com>
2023-09-12 21:48:02 -0500

    routes finished

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App.js'
+import Home from './Routes/Home.js'
+import Contact from './Routes/Contact.js'
+import Favs from './Routes/Favs.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} >
+          <Route path='home' element={<Home />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='favs' element={<Favs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
